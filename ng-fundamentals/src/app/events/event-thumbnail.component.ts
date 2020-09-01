e70648d23cc2bc6c02c12a0fa9a4e7ce61cd2d22
shailendra <shrajp>
2020-09-02 00:27:51 +0530

    ngIf example

--- ng-fundamentals/src/app/events/event-thumbnail.component.ts
+++ ng-fundamentals/src/app/events/event-thumbnail.component.ts
@@ -4,17 +4,21 @@ import {Component,Input, Output, EventEmitter} from '@angular/core'
     selector:'event-thumbnail',
     template:`
         <div class="well hoverwell thumbnail">
-            <h2>{{_eventThumbnail.name}}</h2>
-            <div>Date: {{_eventThumbnail.date}}</div>
-            <div>Time: {{_eventThumbnail.time}}</div>
-            <div>Price: \${{_eventThumbnail.price}}</div>
-            <div>
-                <span>Location: {{_eventThumbnail.location.address}}</span>                
-                <span class="pad-left"> {{_eventThumbnail.location.city}} , {{_eventThumbnail.location.country}}</span>
-            </div>           
+            <h2>{{_eventThumbnail?.name}}</h2>
+            <div>Date: {{_eventThumbnail?.date}}</div>
+            <div>Time: {{_eventThumbnail?.time}}</div>
+            <div>Price: \${{_eventThumbnail?.price}}</div>
+            <div *ngIf="_eventThumbnail?.location">
+                <span>Location: {{_eventThumbnail?.location?.address}}</span>                
+                <span class="pad-left"> {{_eventThumbnail?.location?.city}} , {{_eventThumbnail?.location?.country}}</span>
+            </div>         
+            <div *ngIf="_eventThumbnail?.OnlineUrl">
+            Online Url: {{_eventThumbnail?.OnlineUrl}}
+            </div>  
         </div>
     `,
     styles:[`
+        .thumbnail {min-height: 210px;}
         .pad-left {margin-left: 10px;}
         .well div {color: #bbb ;}
     `]
